06808a729204c201688bab82692e59c75cc4776f
Kazuki Ota <kaota@microsoft.com>
2022-06-16 15:17:32 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorOreoreAuth/Auth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorOreoreAuth.Auth;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private static readonly AuthenticationState UnauthorizedAuthenticationState = new AuthenticationState(new ClaimsPrincipal());
+    private ClaimsPrincipal? _principal;
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        if (_principal is null) return Task.FromResult(UnauthorizedAuthenticationState);
+        return Task.FromResult(new AuthenticationState(_principal));
+    }
+
+    public Task UpdateSignInStatusAsync(ClaimsPrincipal? principal)
+    {
+        _principal = principal;
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        return Task.CompletedTask;
+    }
+}
+
